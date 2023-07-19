26833c3520f0661fcf3c513602726417fd40d810
Saeed Abdulle <104086456+Saabdulle@users.noreply.github.com>
2023-07-19 11:00:13 +0100

    Updated path

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.use(logRoutes);
 app.use("/", express.static(path.join(__dirname, "..", "client")));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "..", "client", "HTML", "welcome.html"));
+    res.sendFile(path.join(__dirname, "..", "public", "HTML", "welcome.html"));
 
 })
 
